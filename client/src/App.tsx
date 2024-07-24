d96d51ec1ac8769070cbd360e1e3d68fb9ac72a1
Guy Cohen <gcohen3@cs.washington.edu>
2024-07-23 18:52:44 -0700

    DWA

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -108,13 +108,13 @@ function App() {
           
         </div>
         <p className='text-sm opacity-60'>
-        because I don't want to pay more for hosting, server can take 50+ seconds to start ):
+        because I don't want to pay more for hosting, server can take 50+ seconds to start ): Try refreshing the page if it takes longer than 5 minutes
         </p>
-        <div className='text-sm flex gap-2 justify-between items-center'>
+        <div className='text-sm flex gap-2 justify-evenly items-center w-full'>
           <p className='text-sm items-center text-center pt-2 flex opacity-60'>On the bright side, you can click on this number as you wait</p>
 
-          <div className='flex flex-col justify-center items-center m-2'>
-            <p className='flex items-center gap-2'>CLICK ME <FaArrowDown size={16} /></p>
+          <div className='flex flex-col justify-center items-center m-2 '>
+            <p className='flex items-center gap-1 pr-1'>CLICK ME <FaArrowDown size={16} /></p>
             <button  
             className="p-2 rounded w-8 h-8 text-4xl animate-bounce"
             onClick={() => setCount(count + 1)}>
