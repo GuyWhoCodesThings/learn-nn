f94632e46cfb58e22f2b82bf6e75e61765296be4
Guy Cohen <gcohen3@cs.washington.edu>
2024-07-26 12:48:49 -0700

    DWA

--- submission.py
+++ submission.py
@@ -1,4 +1,16 @@
 import torch
-class MSE(torch.nn.Module):
-    def forward(self, y_pred: torch.Tensor, y_true: torch.Tensor) -> torch.Tensor:
-        return torch.mean((y_pred - y_true) ** 2)
+class Attention(torch.nn.Module):
+  def __init__(self, model_dim, dk, dv):
+    super().__init__()
+    self.model_dim = model_dim
+    self.Wk = torch.nn.Parameter(torch.randn(model_dim, dk))
+    self.Wq = torch.nn.Parameter(torch.randn(model_dim, dk))
+    self.Wv = torch.nn.Parameter(torch.randn(model_dim, dv))
+
+  def forward(self, q, k, v):
+    Q = q @ self.Wq
+    K = k @ self.Wk
+    V = v @ self.Wv
+    score = (Q @ K.transpose(1, 2)) / self.model_dim ** 0.5
+    score = torch.nn.functional.softmax(score, -1)
+    return score @ V
