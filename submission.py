a72c6e30a0f9e1d09fc86c06427d62f06b0ddc14
Guy Cohen <gcohen3@cs.washington.edu>
2024-09-17 16:12:25 -0700

    changed loading

--- submission.py
+++ submission.py
@@ -1,16 +1,39 @@
 import torch
-class Attention(torch.nn.Module):
-  def __init__(self, model_dim, dk, dv):
+class MultiHeadAttention(torch.nn.Module):
+  def __init__(self, model_dim, num_heads):
     super().__init__()
     self.model_dim = model_dim
-    self.Wk = torch.nn.Parameter(torch.randn(model_dim, dk))
-    self.Wq = torch.nn.Parameter(torch.randn(model_dim, dk))
-    self.Wv = torch.nn.Parameter(torch.randn(model_dim, dv))
+    self.num_heads = num_heads
+    self.dk = model_dim // num_heads
+    
+    self.Wk = torch.nn.Parameter(torch.randn(model_dim, model_dim))
+    self.Wq = torch.nn.Parameter(torch.randn(model_dim, model_dim))
+    self.Wv = torch.nn.Parameter(torch.randn(model_dim, model_dim))
+    self.Wo = torch.nn.Parameter(torch.randn(model_dim, model_dim))
+
+    self.softmax = torch.nn.Softmax(dim=-1)
+
+  def att(self, k, q, v):
+    attention_score = torch.matmul(q, k.transpose(-1,-2)) / self.dk ** 0.5
+    attention_score = self.softmax(attention_score)
+    output = attention_score @ v
+    return output
+
+  def split(self, m):
+    batch_size, seq_len, model_dim = m.shape
+    m = torch.reshape(m, (batch_size, seq_len, self.num_heads, self.dk))
+    m = m.transpose(1, 2)
+    return m
+
+  def combine(self, m):
+    batch_size, num_heads, seq_len, dk = m.shape
+    m = m.transpose(1,2)
+    m = m.reshape(batch_size, seq_len, self.model_dim)
+    return m
 
   def forward(self, q, k, v):
-    Q = q @ self.Wq
-    K = k @ self.Wk
-    V = v @ self.Wv
-    score = (Q @ K.transpose(1, 2)) / self.model_dim ** 0.5
-    score = torch.nn.functional.softmax(score, -1)
-    return score @ V
+    Q = self.split(q @ self.Wq)
+    K = self.split(k @ self.Wk)
+    V = self.split(v @ self.Wv)
+    out = self.att(K, Q, V)
+    return self.combine(out) @  self.Wo
